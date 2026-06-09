4656a93467ac39ecacd62c831ef735dbae0f2904
Seum <mostofaseum8@gmail.com>
2026-06-09 14:22:33 +0600

    feat: implement authentication client UI and server actions for sign-in and sign-up flows

--- starter-code/lib/actions/auth-actions.ts
+++ starter-code/lib/actions/auth-actions.ts
@@ -2,6 +2,7 @@
 
 import { headers } from "next/headers";
 import { auth } from "../auth";
+import { redirect } from "next/navigation";
 
 export const signUp = async(name:string, email:string, password:string) => {
     const result = await auth.api.signUpEmail({
@@ -26,6 +27,18 @@ export const signIn = async(password:string, email:string) => {
     });
 
     return result;
+}
+
+export const signInSocial = async(provider: "google" | "github") => {
+    const {url} = await auth.api.signInSocial({
+        body:{
+            provider,
+            callbackURL:"/dashboard",
+        },
+    });
+if(url){
+    redirect(url);
+}
 }
 
 export const signOut = async() => {
